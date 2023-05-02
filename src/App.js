4fe0e69ea2bcb57351dac8dad1c8aa428ef7de08
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-05-02 19:49:31 +0300

    project setup

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import "/node_modules/bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+
+
+const displayFormInput = () => {
+
+}
+
+const Home = () => {
+    return (
+
+    <div className="sidebar-section">
+        <div className="sidebar-section-title" onClick={displayFormInput}>
+            <h2>KEYWORD</h2>
+            <div>arrow icon</div>
+        </div>
+        {/* search input... */}
+    </div>
+
+
+
+    )
+}
