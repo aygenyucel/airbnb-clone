4fe6755588d420e21495aa4d5fe9816f68aabbe2
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2023-05-24 13:44:04 +0300

    adds carousel for place images & styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,13 +1,37 @@
 import "./card.scss";
 import {cards} from "../../assets/cards-list.js";
+import { Carousel } from "react-bootstrap";
 
 const Card = () => {
-
+    const slideImages = [
+        "https://a0.muscache.com/im/pictures/miso/Hosting-668146487515150072/original/8ff2a532-e0cd-41a2-9164-554c4d9eb28a.jpeg?im_w=960",
+        "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lpibo-ew-1656015868.jpg",
+        "https://www.travelandleisure.com/thmb/BJupPeakYV7RY_vQQnmvrKAl7LU=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/soneva-jani-sunset-view-maldives-SONEVA0421-74b37591d80441ce87831a41da518e49.jpg",
+        "https://i0.wp.com/theluxuryeditor.com/wp-content/uploads/2019/02/178793622.jpg?resize=1024%2C683",
+      ];
+      
     return (
         <div className="place-card d-flex flex-column">
-            <div className="card-img">
+            {/* <div className="card-img">
                 <img src="https://a0.muscache.com/im/pictures/miso/Hosting-668146487515150072/original/8ff2a532-e0cd-41a2-9164-554c4d9eb28a.jpeg?im_w=960" alt="" />
-            </div>
+
+            </div> */}
+            <Carousel interval={null}>
+                {
+                    slideImages.map((img, index) => (
+                        <Carousel.Item>
+                            <div className="card-img">
+                                <img
+                                className="d-block w-100"
+                                src={img}
+                                alt="First slide"
+                                />
+                            </div>
+                        </Carousel.Item>
+                    ))
+                }
+            </Carousel>
+            
             <div className="card-info d-flex flex-column">
                 <div className="d-flex justify-content-between">
                     <div className="card-info-location">
